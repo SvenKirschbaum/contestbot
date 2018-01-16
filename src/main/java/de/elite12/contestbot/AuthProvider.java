18cdbe608b2fe35c688a0fcdf514ef4e69e81358
Sven Kirschbaum <sven@elite12.de>
2018-01-16 23:27:39 +0100

    Cache size increased

--- src/main/java/de/elite12/contestbot/AuthProvider.java
+++ src/main/java/de/elite12/contestbot/AuthProvider.java
@@ -10,18 +10,18 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
 public class AuthProvider {
-    private static LoadingCache<String, Boolean> cache = CacheBuilder.newBuilder().concurrencyLevel(2).maximumSize(40)
-            .expireAfterWrite(24, TimeUnit.HOURS).build(new CacheLoader<String, Boolean>() {
+    private static LoadingCache<String, Boolean> cache = CacheBuilder.newBuilder().concurrencyLevel(2)
+            .maximumSize(1000000).expireAfterWrite(24, TimeUnit.HOURS).build(new CacheLoader<String, Boolean>() {
                 @Override
                 public Boolean load(String key) throws Exception {
                     return Boolean.FALSE;
                 };
             });
-
+    
     public static void MarkPrivileged(String user) {
         cache.put(user.toLowerCase(), Boolean.TRUE);
     }
-    
+
     public static Boolean checkPrivileged(String user) {
         try {
             return cache.get(user.toLowerCase());
